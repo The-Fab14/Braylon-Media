9c0a90f36dc6cbbf623eb0bc060de47ba79f23de
dc527x <43080004+dc527x@users.noreply.github.com>
2020-03-17 15:37:25 -0400

    Deleted?

--- src/main/java/com/dev10/BraylonMedia/config/SecurityWebAppInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.dev10.BraylonMedia.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
- class SecurityWebApplicationInitializer 
-                               extends AbstractSecurityWebApplicationInitializer {
-    
-}
