70253029cc0fbc968bb15a1adee291ccc2b2bebf
Lindsay Nickalo <bradleyl4@winthrop.edu>
2020-03-18 11:58:21 -0400

    Started Controller Implementation

--- src/main/java/com/dev10/BraylonMedia/controllers/HomeController.java
+++ src/main/java/com/dev10/BraylonMedia/controllers/HomeController.java
@@ -1,11 +1,28 @@
 package com.dev10.BraylonMedia.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * @author Lindsay Gen10
  * @date Mar 18, 2020
  */
+
+@Controller
 public class HomeController 
 {
-
+    @GetMapping({"/", "/home"})
+    public String displayHome(Model model, HttpServletRequest request)
+    {
+        String userId = request.getUserPrincipal().getName();
+        //needs list of client objects associated with current user's id
+        //list of all clients if role is admin
+        model.addAttribute("userId", userId);
+        //List<Client> clients = function to get clients;
+        //model.addAttribute("clients", clients);
+        return "home";
+    }
 }
--- src/main/java/com/dev10/BraylonMedia/controllers/OrderController.java
+++ src/main/java/com/dev10/BraylonMedia/controllers/OrderController.java
@@ -1,10 +1,14 @@
 package com.dev10.BraylonMedia.controllers;
 
+import org.springframework.stereotype.Controller;
+
 /**
  *
  * @author Lindsay Gen10
  * @date Mar 18, 2020
  */
+
+@Controller
 public class OrderController 
 {
 
