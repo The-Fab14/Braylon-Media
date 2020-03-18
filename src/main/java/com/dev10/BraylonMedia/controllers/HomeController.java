8c882583d6ecc281ed1fa344d42bc451f0236533
Lindsay Nickalo <bradleyl4@winthrop.edu>
2020-03-18 14:43:24 -0400

    fixed mapping

--- src/main/java/com/dev10/BraylonMedia/controllers/HomeController.java
+++ src/main/java/com/dev10/BraylonMedia/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -30,6 +31,7 @@ public class HomeController
         return "home";
     }
     
+    @ResponseBody
     @GetMapping("/clients/{userId}")
     public List<Client> getClients(@PathVariable String userId)
     {
