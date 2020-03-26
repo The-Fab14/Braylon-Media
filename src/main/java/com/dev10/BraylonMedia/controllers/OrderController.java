bf80dc56cb6ee98861d45c19d2435efb99610f2b
Lindsay Nickalo <bradleyl4@winthrop.edu>
2020-03-26 15:08:18 -0400

    Daniel's and Joe's add order stuff

--- src/main/java/com/dev10/BraylonMedia/controllers/OrderController.java
+++ src/main/java/com/dev10/BraylonMedia/controllers/OrderController.java
@@ -7,9 +7,13 @@ import com.dev10.BraylonMedia.entities.User;
 import com.dev10.BraylonMedia.services.ClientService;
 import com.dev10.BraylonMedia.services.OrderService;
 import com.dev10.BraylonMedia.services.UserService;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,6 +30,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class OrderController {
 
     Set<ConstraintViolation<User>> violations = new HashSet<>();
+    Set<String> customViolations = new HashSet<>();    
 
     @Autowired
     OrderService orderService;
@@ -37,8 +42,99 @@ public class OrderController {
     UserService userService;
 
     @GetMapping("/add_new_order")
-    public String displayAddOrder() {
+    public String displayAddOrder(Model model) {
+        model.addAttribute("customViolations", customViolations);
         return "add_new_order";
+    }
+
+    @PostMapping("/add_new_order")
+    public String addCustomer(HttpServletRequest request, Model model, String dateSubmitted,
+            String dateInstalled, String dateCompleted, String orderTotal,
+            String orderStatus, String orderComments, String clientId, String productId, String productQuantity) {
+
+        LocalDate dateSubmittedLocalDate;
+        LocalDate dateInstalledLocalDate;
+        LocalDate dateCompletedLocalDate;
+        BigDecimal orderTotalBigDecimal;
+        int clientIdInt;
+        int productIdInt;
+        int productQuantityInt;
+
+        try {
+            dateSubmittedLocalDate = LocalDate.parse(dateSubmitted);
+        } catch (Exception e) {
+            customViolations.add("Date submitted format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+
+        try {
+            dateInstalledLocalDate = LocalDate.parse(dateInstalled);
+        } catch (Exception e) {
+            customViolations.add("Date installed format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+
+        try {
+            dateCompletedLocalDate = LocalDate.parse(dateCompleted);
+        } catch (Exception e) {
+            customViolations.add("Date completed format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+
+        try {
+            orderTotalBigDecimal = new BigDecimal(orderTotal);
+        } catch (Exception e) {
+            customViolations.add("Order total format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+
+        try {
+            clientIdInt = Integer.parseInt(clientId);
+        } catch (Exception e) {
+            customViolations.add("Client Id format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+
+        try {
+            productIdInt = Integer.parseInt(productId);
+        } catch (Exception e) {
+            customViolations.add("Product Id format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+        
+        try {
+            productQuantityInt = Integer.parseInt(productQuantity);
+        } catch (Exception e) {
+            customViolations.add("Product quantity format is incorrect.");
+            return "redirect:/add_new_order";
+        }
+
+        User userId = userService.getUserFromSession();
+        Order order = new Order();
+        order.setDateSubmitted(dateSubmittedLocalDate);
+        order.setDateInstalled(dateInstalledLocalDate);
+        order.setDateCompleted(dateCompletedLocalDate);
+        order.setOrderTotal(orderTotalBigDecimal);
+        order.setOrderStatus(orderStatus);
+        order.setOrderComments(orderComments);
+
+        Client client = new Client();
+        client.setClientId(clientIdInt);
+        order.setClient(client);
+
+        List<Product> productList = new ArrayList<>();
+
+        Product product = new Product();
+        product.setProductId(productIdInt);
+        productList.add(product);
+        order.setProducts(productList);
+
+        order = orderService.addOrder(order);
+        
+        orderService.saveOrderProductQuantity(order.getOrderId(), productIdInt, productQuantityInt);
+        
+        customViolations.clear();
+        return "redirect:/home";
     }
 
     @GetMapping("/edit_order")
@@ -66,13 +162,6 @@ public class OrderController {
 
         return "edit_order";
 
-    }
-
-    @PostMapping("/edit_order")
-    public String editOrder(Order order, Integer productId) 
-    {
-        orderService.saveOrderProductQuantity(order.getOrderId(), productId, 0);
-        return "/orders";
     }
     
     @GetMapping("/orders")
