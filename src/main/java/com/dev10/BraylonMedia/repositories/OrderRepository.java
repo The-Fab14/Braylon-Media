3f3b2a9898ac9bb4c1560b5847c464d3a4210baa
Daniel <45375461+dan403s@users.noreply.github.com>
2020-03-27 10:18:16 -0400

    Update OrderRepository.java

--- src/main/java/com/dev10/BraylonMedia/repositories/OrderRepository.java
+++ src/main/java/com/dev10/BraylonMedia/repositories/OrderRepository.java
@@ -11,8 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
-
+import javax.transaction.Transactional;
 /**
  *
  * @author diego
